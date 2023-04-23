1d8ab9635bc3e6503cd280735b46bb7b88bb8c9e
ainaUwU <ainacrespiv@gmail.com>
2023-04-23 22:31:06 +0200

    Anim y audio

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class InventorySlot
@@ -10,8 +11,25 @@ public class InventorySlot
 
     public void UpdateSlotUI()
     {
-        // Código para actualizar la interfaz de usuario de la ranura de inventario
-    }
+        // actualizar la interfaz de usuario de la ranura de inventario
+        if (slotUI != null)
+        {
+            // busca los componentes de imagen y texto que representan el icono y el nombre del objeto de inventario
+            Image slotImage = slotUI.transform.Find("ItemImage").GetComponent<Image>();
+            Text slotText = slotUI.transform.Find("ItemText").GetComponent<Text>();
 
-    // Otros métodos y propiedades de la clase InventorySlot
+            if (item != null)
+            {
+                slotImage.sprite = item.itemIcon;
+                slotImage.enabled = true;
+                slotText.text = item.itemName;
+            }
+            else
+            {
+                slotImage.sprite = null;
+                slotImage.enabled = false;
+                slotText.text = "";
+            }
+        }
+    }
 }
