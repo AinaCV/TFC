2568916baef5c52a3842920a9615c0490343df72
ainaUwU <ainacrespiv@gmail.com>
2023-05-07 14:36:16 +0200

    INK and Dialogue

---
+++ Assets/Scripts/DialogSystem/DialogueManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+
+    [SerializeField] private TextMeshProUGUI dialogueText; //using TMPro;
+
+    private Story currentStory; //using Ink.Runtime;
+
+    private bool dialogueIsPlaying;
+
+    private static DialogueManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Dialogue Manager in the scene");
+        }
+        instance = this;
+    }
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)//coge el json con los dialogos
+    {
+        currentStory = new Story(inkJSON.text);//se inicializa con la info del json
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = ""; // dejamos el texto en un string vacia por si acaso
+    }
+
+    void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();//nos da la siguiente linea de dialogo y además la quita de la lista de lineas que pueden salir en el dialogo
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+}
